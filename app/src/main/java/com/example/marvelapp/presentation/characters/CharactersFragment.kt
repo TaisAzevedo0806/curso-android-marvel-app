72bb9954e34cb4abd1d57e66c895cb270c76617d
taisa <taisazevedo@hotmail.com>
2022-05-23 11:12:09 -0300

    04.04 - Implement characters fragment.

--- app/src/main/java/com/example/marvelapp/presentation/characters/CharactersFragment.kt
+++ app/src/main/java/com/example/marvelapp/presentation/characters/CharactersFragment.kt
@@ -5,15 +5,52 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.core.domain.model.Character
 import com.example.marvelapp.R
+import com.example.marvelapp.databinding.FragmentCharactersBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class CharactersFragment : Fragment() {
+
+    private var _binding: FragmentCharactersBinding? = null
+    private val binding: FragmentCharactersBinding get() = _binding!!
+
+    /**
+     *  By instantiating the property outside the lifecycle methods,
+     *  the value is not lost when navigating to other screens.
+     */
+    private val charactersAdapter = CharactersAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_characters, container, false)
+    ) = FragmentCharactersBinding.inflate(inflater, container, false).apply {
+        _binding = this
+    }.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initCharactersAdapter()
+
+        charactersAdapter.submitList(
+            listOf(
+                Character("3D-Man", "https://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg"),
+                Character("3D-Man", "https://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg"),
+                Character("3D-Man", "https://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg"),
+                Character("3D-Man", "https://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg"),
+                Character("3D-Man", "https://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg"),
+                Character("3D-Man", "https://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg"),
+                Character("3D-Man", "https://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg"),
+                Character("3D-Man", "https://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg")
+            )
+        )
     }
 
+    private fun initCharactersAdapter() {
+        with(binding.recyclerCharacters) {
+            setHasFixedSize(true)
+            adapter = charactersAdapter
+        }
+    }
 }
